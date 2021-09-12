eccc8aa29dce7d3c925cfaad7b612ea50d2ffaf0
cheddarl <cheddarlord12@gmail.com>
2021-09-12 23:11:42 +0000

    React Hooks - 1209
    Signed-off-by: cheddarl <cheddarlord12@gmail.com>

--- hooks/src/components/layout/Menu.jsx
+++ hooks/src/components/layout/Menu.jsx
@@ -7,22 +7,19 @@ const Menu = props => (
         <nav>
             <ul>
                 <li> 
-                    <Link to="/">Home</Link>
+                    <Link to="/">Hooks</Link>
                 </li>
                 <li> 
-                    <Link to="/conteudo01/123">Conteúdo 01</Link>
+                    <Link to="/useState">useState()</Link>
                 </li>
                 <li> 
-                    <Link to="/conteudo02">Conteúdo 02</Link>
+                    <Link to="/useEffect">useEffect()</Link>
                 </li>                                
                 <li> 
-                    <Link to="/conteudo03">Conteúdo 03</Link>
-                </li>
-                <li> 
-                    <Link to="/conteudo04">Conteúdo 04</Link>
+                    <Link to="/useContext">useContext()</Link>
                 </li>                                
             </ul>
         </nav>
     </aside>
 )
-export default Menu
+export default Menu;
---
+++ hooks/src/views/contents/Hooks.jsx
@@ -0,0 +1,16 @@
+import './Hooks.css'
+import React from "react"
+
+const Hooks = props => (
+    <div className="container">
+        <div className="titulo">
+            <h1>React Hooks</h1>
+        </div>
+
+        <div className="conteudo">
+            <h2><a href="https://pt-br.reactjs.org/docs/hooks-intro.html">Documentação</a></h2>
+        </div>
+    </div>
+)
+
+export default Hooks
